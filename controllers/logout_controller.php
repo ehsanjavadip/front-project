31bdd4d5bbaa184767e832d4187cf1391649f189
Ehsan Javadi <54818769+ehsanjavadip@users.noreply.github.com>
2022-04-29 19:56:44 +0430

    Delete controllers directory

--- controllers/logout_controller.php
+++
@@ -1,8 +0,0 @@
-<?php 
-  include_once '../SQL/dbconnection.php';
-  
-  if(isset($_SESSION['loggedin'])) {
-    unset($_SESSION['loggedin']);
-    header("location: ../views/login_view.php");
-  } else {header("location: ../login_view.php");}
-?>
